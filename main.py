36cf9877d14bbe9ceff533854b13aa4978af03fe
phadziewicz <you@example.com>
2021-05-12 16:47:33 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# x = np.arange(1, 21, 1)
+# plt.plot(x, 1/x, label='f(x)=1/x')
+# plt.axis([0, 20, 0, 1])
+# plt.xlabel('x')
+# plt.ylabel('f(x)')
+# plt.title('Wykres funkcji f(x) dla x [1, 20]')
+# plt.legend()
+# plt.show()
+
+# zad2
+# x = np.arange(1, 21, 1)
+# plt.plot(x, 1/x, 'g>', linestyle=':', label='f(x)=1/x')
+# plt.axis([0, 20, 0, 1])
+# plt.xlabel('x')
+# plt.ylabel('f(x)')
+# plt.title('Wykres funkcji f(x) dla x [1, 20]')
+# plt.legend()
+# plt.show()
+
+# zad3 i zad4
+# x1 = np.arange(0, 30, 0.1)
+# x2 = np.arange(0, 30, 0.1)
+# y1 = np.sin(0.317 * np.pi * x1 + 3)
+# y2 = np.cos(2 * np.pi * x2)
+# y3 = np.sin(0.317 * np.pi * x1) + 2
+# plt.subplot(2, 1, 1)
+# plt.plot(x1, y1, '-', label='sin(x)')
+# plt.plot(x1, y3, 'g-', label='sin(x)')
+# plt.title('Wykres sin(x) sin(x)')
+# plt.ylabel('sin(x)')
+# plt.legend()
+# plt.subplot(2, 1, 2)
+# plt.plot(x2, y2, 'r-', label='cos(x)')
+# plt.xlabel('x')
+# plt.ylabel('cos(x)')
+# plt.legend()
+# plt.show()
